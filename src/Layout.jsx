6074f2448a5472ff0c813b4087f3ad11315a34bb
Md Wasim Ansari <119097409+md-wasim-123@users.noreply.github.com>
2024-01-18 13:05:10 +0530

    hello fixed

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './util/Header/Header'
- import Footer from './util/Footer/Footer'
+import Footer from './util/footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
